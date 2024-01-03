fd89f179587f456b4c8404a9d833a67cc3ae52ff
wxf <wxf4150@163.com>
2024-01-03 22:39:35 +0800

    disable secp256k1 for cgo

--- pkg/keystore/recover.go
+++ pkg/keystore/recover.go
@@ -3,26 +3,27 @@
 
 package keystore
 
-import (
-	"github.com/ethereum/go-ethereum/crypto/secp256k1"
-	"github.com/fbsobreira/gotron-sdk/pkg/address"
-)
-
-func RecoverPubkey(hash []byte, signature []byte) (address.Address, error) {
-
-	if signature[64] >= 27 {
-		signature[64] -= 27
-	}
-
-	sigPublicKey, err := secp256k1.RecoverPubkey(hash, signature)
-	if err != nil {
-		return nil, err
-	}
-	pubKey, err := UnmarshalPublic(sigPublicKey)
-	if err != nil {
-		return nil, err
-	}
-
-	addr := address.PubkeyToAddress(*pubKey)
-	return addr, nil
-}
+//
+//import (
+//	"github.com/ethereum/go-ethereum/crypto/secp256k1"
+//	"github.com/fbsobreira/gotron-sdk/pkg/address"
+//)
+//
+//func RecoverPubkey(hash []byte, signature []byte) (address.Address, error) {
+//
+//	if signature[64] >= 27 {
+//		signature[64] -= 27
+//	}
+//
+//	sigPublicKey, err := secp256k1.RecoverPubkey(hash, signature)
+//	if err != nil {
+//		return nil, err
+//	}
+//	pubKey, err := UnmarshalPublic(sigPublicKey)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	addr := address.PubkeyToAddress(*pubKey)
+//	return addr, nil
+//}
